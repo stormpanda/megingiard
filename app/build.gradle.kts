166fe823d38eed800dce71cc1a1b6632e5ffbb1f
stormpanda <maikthomalla@gmail.com>
2026-05-14 15:36:23 +0200

    chore: set version to 0.1.0 and configure release APK filename
    - Set versionName to "0.1.0" in app/build.gradle.kts
    - Add applicationVariants output renaming: release APK → Megingiard-v<versionName>.apk
    - add app/release to .gitignore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 33
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -29,6 +29,16 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+        }
+    }
+
+    applicationVariants.all {
+        val variant = this
+        variant.outputs.all {
+            val output = this as com.android.build.gradle.internal.api.BaseVariantOutputImpl
+            if (variant.buildType.name == "release") {
+                output.outputFileName = "Megingiard-v${variant.versionName}.apk"
+            }
         }
     }
     compileOptions {
