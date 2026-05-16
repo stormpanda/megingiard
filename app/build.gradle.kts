5d337a24aace515d618096fd11b5f043e51f5be6
stormpanda <maikthomalla@gmail.com>
2026-05-16 18:49:52 +0200

    chore(versioning): raise app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 33
         targetSdk = 35
         versionCode = 1
-        versionName = "0.1.0"
+        versionName = "0.2.0-SNAPSHOT"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
