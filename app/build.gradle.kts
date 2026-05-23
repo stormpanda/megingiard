ef78b444fced962d14546b8a5afe5d7eebc952b2
stormpanda <maikthomalla@gmail.com>
2026-05-23 16:03:27 +0200

    chore(release): set version to 0.2.0 for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
         minSdk = 33
         targetSdk = 35
         versionCode = 1
-        versionName = "0.2.0-SNAPSHOT"
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
