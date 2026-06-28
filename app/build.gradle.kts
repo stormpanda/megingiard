8338ab439962275d641ac11e14c15044184b3f10
stormpanda <maikthomalla@gmail.com>
2026-06-28 14:46:24 +0200

    chore(release): set version name to 0.5.0 for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
         minSdk = 33
         targetSdk = 35
         versionCode = 5
-        versionName = "0.5.0-SNAPSHOT"
+        versionName = "0.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
