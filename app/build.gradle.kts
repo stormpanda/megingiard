5ee1d1022cc7839a9245a6dc58b739cf0c3b2bb2
stormpanda <maikthomalla@gmail.com>
2026-07-16 08:50:54 +0200

    chore(release): set version code to 8 and version name to 0.7.0-SNAPSHOT for development

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         applicationId = "com.stormpanda.megingiard"
         minSdk = 33
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.6.1"
+        versionCode = 8
+        versionName = "0.7.0-SNAPSHOT"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
