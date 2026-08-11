e50f4d1c2ae0878cc5a1fb4a870b1b61443f83a3
stormpanda <maikthomalla@gmail.com>
2026-08-11 19:10:01 +0200

    chore(release): set version name to 0.8.0 for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
         minSdk = 33
         targetSdk = 35
         versionCode = 8
-        versionName = "0.8.0-SNAPSHOT"
+        versionName = "0.8.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
