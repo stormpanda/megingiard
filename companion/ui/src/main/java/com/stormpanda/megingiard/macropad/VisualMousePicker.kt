3a3eea187463ddc655801e1a1a5aed528d570577
stormpanda <maikthomalla@gmail.com>
2026-08-31 14:50:20 +0200

    refactor(macropad): remove unused remember import in VisualMousePicker

--- companion/ui/src/main/java/com/stormpanda/megingiard/macropad/VisualMousePicker.kt
+++ companion/ui/src/main/java/com/stormpanda/megingiard/macropad/VisualMousePicker.kt
@@ -5,7 +5,6 @@ import androidx.compose.material.icons.rounded.ControlCamera
 import androidx.compose.material.icons.rounded.Mouse
 import androidx.compose.material.icons.rounded.SwapVert
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
